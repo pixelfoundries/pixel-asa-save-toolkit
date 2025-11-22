9ce29cde1b92d8cfd736ad993b93572ee99e6ae6
Doug Kasten <dkasten79@gmail.com>
2025-11-21 23:26:29 -0700

    chore: reenable browser file support and add ignore errors for browser support

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,19 +32,19 @@ tsc -p tsconfig.json
 # Clone files for node builds
 cp -pr "$DIST_DIR" "$NODE_DIST_DIR"
 
-## Remove browser files in node build
-#for FILE in ${NODE_DIST_DIR}/*-browser*; do
-#  rm -f $FILE
-#done
-#
-## Move browser files into place for default build
-#(
-#  cd "$DIST_DIR"
-#
-#  for FILE in *-browser*;do
-#    mv "$FILE" "${FILE/-browser/}"
-#  done
-#)
+# Remove browser files in node build
+for FILE in ${NODE_DIST_DIR}/*-browser*; do
+  rm -f $FILE || true
+done
+
+# Move browser files into place for default build
+(
+  cd "$DIST_DIR"
+
+  for FILE in *-browser*;do
+    mv "$FILE" "${FILE/-browser/}" || true
+  done
+)
 
 # Remove type definition files where they're not needed
 (
